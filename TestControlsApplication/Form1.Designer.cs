db017f2257c21d9b0479e45541139fadda317ee6
limonpudding <martyshenko.sergei@yandex.ru>
2018-09-20 23:38:54 +0500

    Сделаны часы

--- TestControlsApplication/Form1.Designer.cs
+++ TestControlsApplication/Form1.Designer.cs
@@ -30,21 +30,33 @@
         {
             this.components = new System.ComponentModel.Container();
             this.numberBox1 = new WinFormsControlLab.NumberBox(this.components);
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.userControl11 = new WinFormsControlLab.UserControl1();
             this.SuspendLayout();
             // 
             // numberBox1
             // 
             this.numberBox1.ForeColor = System.Drawing.Color.Red;
-            this.numberBox1.Location = new System.Drawing.Point(180, 107);
+            this.numberBox1.Location = new System.Drawing.Point(12, 12);
             this.numberBox1.Name = "numberBox1";
             this.numberBox1.Size = new System.Drawing.Size(100, 20);
             this.numberBox1.TabIndex = 0;
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(331, 113);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(407, 295);
+            this.elementHost1.TabIndex = 1;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.userControl11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 237);
+            this.ClientSize = new System.Drawing.Size(879, 487);
+            this.Controls.Add(this.elementHost1);
             this.Controls.Add(this.numberBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +68,8 @@
         #endregion
 
         private WinFormsControlLab.NumberBox numberBox1;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private WinFormsControlLab.UserControl1 userControl11;
     }
 }
 
---
+++ WinFormsControlLab/UserControl1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinFormsControlLab
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        System.Windows.Threading.DispatcherTimer timer;
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            second.RenderTransform = new RotateTransform(DateTime.Now.Second * 6);
+            minute.RenderTransform = new RotateTransform(DateTime.Now.Minute * 6);
+            hour.RenderTransform = new RotateTransform(DateTime.Now.Hour * 30);
+        }
+    }
+}
